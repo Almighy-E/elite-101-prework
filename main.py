8c079525599628525be9d090b1d4b7982a46e573
Almighy-E <rosalynsatori@gmail.com>
2025-09-20 03:36:10 +0000

    choice list

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 print("Welcome to the food ordering Chatbot")
 name = input("Please input your name: ")
-age = input("How old are you " + name + "?")
+age = input("How old are you " + name + "? ")
+menu = ["Browse food", "Browse Drinks", "exit menu"]
+choice = input("How can I help you? (Browse food, Browse Drinks, exit menu)")
